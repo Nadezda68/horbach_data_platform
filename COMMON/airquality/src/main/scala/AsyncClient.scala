61525dd2aac49888153151c1dd24a08c67910e2a
Ariane <ariane.horbach@dpdhl.com>
2025-03-18 18:03:52 +0100

    [AQ] Adding Swagger Client to Porject

---
+++ COMMON/airquality/src/main/scala/AsyncClient.scala
@@ -0,0 +1,16 @@
+import com.wordnik.swagger.client._
+
+import java.io.Closeable
+
+class AsyncClient(config: SwaggerConfig) extends Closeable {
+  lazy val locator: ServiceLocator = config.locator
+  lazy val name: String = config.name
+
+  private[this] val client = transportClient
+
+  protected def transportClient: TransportClient = new RestClient(config)
+
+  def close() {
+    client.close()
+  }
+}
